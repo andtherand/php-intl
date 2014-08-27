ea1b3334eb2d03047b864f9cd5ea05045a5dae6b
Andy Ruck <mychiara+github@gmail.com>
2014-08-27 15:35:47 +0200

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name		  	      "php-intl"
+maintainer        "mychiara"
+maintainer_email  "mychiara+cookbooks@gmail.com"
+license           "Apache 2.0"
+description       "installs php intl extension"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "0.0.1"
+recipe            "php-intl", "installs php intl extension"
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
+
+
